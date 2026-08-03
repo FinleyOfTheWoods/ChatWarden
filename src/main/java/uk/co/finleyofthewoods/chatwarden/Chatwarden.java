7e7d872da5c6906e0e56aa5a5268f4be5359fbb5
FinleyOfTheWoods <finley.of.the.woods@gmail.com>
2026-08-03 08:45:47 +0100

    initial commit

---
+++ src/main/java/uk/co/finleyofthewoods/chatwarden/Chatwarden.java
@@ -0,0 +1,10 @@
+package uk.co.finleyofthewoods.chatwarden;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chatwarden implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
